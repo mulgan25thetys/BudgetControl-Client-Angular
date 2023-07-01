1a3a9134da18b621712c97cd56557ce694bef866
mulgan25thetys <mulganthetys.ngougbiamayele@esprit.tn>
2023-07-01 17:43:27 +0100

    Implementing capital and devise services

--- src/app/services/devise.service.ts
+++ src/app/services/devise.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Devise } from '../models/devise';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,29 @@ export class DeviseService {
     )
   }
 
-  constructor(private http:HttpClient) { }
+  api_url = '/api/api/devises/'
+
+  constructor(private http: HttpClient) { }
+  
+  getAllDevises(): Observable<Devise[]> {
+    return this.http.get<Devise[]>(this.api_url)
+  }
+  getDefault(): Observable<any> {
+    return this.http.get<any>(this.api_url + 'default')
+  }
+  getOne(id: number): Observable<any> {
+    return this.http.get<any>(this.api_url + id)
+  }
+  getAllRegions(): Observable<any[]> {
+    return this.http.get<any[]>(this.api_url+'regions')
+  }
+  deleteDevise(id :number): Observable<any>{
+    return this.http.delete<any>(this.api_url+id)
+  }
+  addDevise(devise:Devise): Observable<any>{
+    return this.http.post<any>(this.api_url+'new', devise,this.httpOptions)
+  }
+  setDefault(id: number): Observable<any>{
+    return this.http.put<any>(this.api_url+id, null,this.httpOptions)
+  }
 }
